270c99de21ba8278fc6674ca4a97f7ee4b76d771
GreNaz <nazargrehov@gmail.com>
2023-01-22 17:32:55 +0300

    up model

--- src/main/java/ru/practicum/shareit/user/model/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/model/dto/UserDto.java
@@ -5,7 +5,8 @@ import ru.practicum.shareit.error.validation.Create;
 import ru.practicum.shareit.error.validation.Update;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
@@ -15,10 +16,10 @@ import javax.validation.constraints.NotNull;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class UserDto {
     private long id;
-    @NotNull(groups = Create.class, message = "Received user with empty name")
+    @NotBlank(groups = Create.class, message = "Received user with empty name")
     private String name;
     @Email(groups = {Create.class, Update.class}, message = "Incorrect email address")
-    @NotNull(groups = Create.class, message = "Email not specified")
+    @NotEmpty(groups = Create.class, message = "Email not specified")
     @EqualsAndHashCode.Include
     private String email;
 }
